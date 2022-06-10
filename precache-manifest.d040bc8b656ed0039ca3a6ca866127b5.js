4b0d6bd88e424224064cf050b18bcce907e0b425
nidhishdb <nidhishdb@gmail.com>
2022-06-10 19:46:15 +0530

    Updates

---
+++ precache-manifest.d040bc8b656ed0039ca3a6ca866127b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cda1eddef5e4a187a45",
+    "url": "/facedetect/static/css/main.fef5ebdb.chunk.css"
+  },
+  {
+    "revision": "0cda1eddef5e4a187a45",
+    "url": "/facedetect/static/js/main.0cda1edd.chunk.js"
+  },
+  {
+    "revision": "c11ba39c68b0ab989f9d",
+    "url": "/facedetect/static/css/1.0a87fe3a.chunk.css"
+  },
+  {
+    "revision": "c11ba39c68b0ab989f9d",
+    "url": "/facedetect/static/js/1.c11ba39c.chunk.js"
+  },
+  {
+    "revision": "862a8f2528d7fb502170",
+    "url": "/facedetect/static/js/runtime~main.862a8f25.js"
+  },
+  {
+    "revision": "d915af3a72137815ccf6946e3dad82ba",
+    "url": "/facedetect/index.html"
+  }
+];
